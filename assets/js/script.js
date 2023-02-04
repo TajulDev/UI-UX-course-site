a22d64235c9a8bf1f1acbabebf1af51925c6dd43
jahanabir8 <abir.opediatech@gmail.com>
2023-02-04 17:23:24 +0600

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,10 @@ const mobileMenu = document.querySelector(".mobile__menu");
 const cross = document.querySelector(".crossMark");
 
 menubar.addEventListener("click", ()=>{
-    mobileMenu.classList.toggle("d-block")
+    mobileMenu.classList.toggle("open")
 })
 cross.addEventListener("click", ()=>{
-        mobileMenu.classList.remove("d-block")
+        mobileMenu.classList.remove("open")
 })
 // menu script
 
@@ -15,7 +15,6 @@ var input = document.querySelector("#phone");
 var iti = window.intlTelInput(input, {
   // separateDialCode:true,
   utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@17.0.3/build/js/utils.js",
-});
-
+}); 
 // store the instance variable so we can access it in the console e.g. window.iti.getNumber()
 window.iti = iti;
